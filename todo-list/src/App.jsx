53d9f462fa806f40e95b2d8ba24ee7725e0f8ddc
Ahmetdlpr <mybqadresadrs@outlook.com>
2024-10-21 22:12:50 +0300

    Arayüz değişiklikleri yapıldı

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+  
 
-  return (
-      <>
-        <h1>Tod do list project</h1>
-    </>
+
+  
+
+    return (
+        <div className="App">
+
+            <div className="Walcomecontainer">
+
+
+            </div>
+
+            <h2>Yapilacaklar listesi</h2>
+
+
+            <div className="input-container">
+
+                <form>
+                    <h2>Listeye eleman ekleyin</h2>
+                    <input className="input" type="text"></input>
+                    <button onClick={addbutton} href="#" onSubmit="none" className="addbutton">Ekle</button>
+
+                </form>
+
+
+                <ul className="List">
+                    <li>Liste1 <a className="Deletebutton">-</a><a className="Dbutton">+</a></li>
+                  
+                </ul>
+
+
+            </div>
+
+        </div>
   )
 }
 
